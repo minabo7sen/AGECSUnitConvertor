c6cbc799db3a3944a4156e7a40d08c7a376e67ff
Mina Mohsen <minamhakim@gmail.com>
2020-12-06 13:07:22 +0200

    :lipstick: Improved Menu dynamics

--- AGECSUnitConvertor/PageModels/Convertors/MenuPageModel.cs
+++ AGECSUnitConvertor/PageModels/Convertors/MenuPageModel.cs
@@ -2,6 +2,7 @@
 using PropertyChanged;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,16 +13,17 @@ namespace AGECSUnitConvertor.PageModels.Convertors
     [AddINotifyPropertyChangedInterface]
     public class MenuPageModel : FreshBasePageModel, INotifyPropertyChanged
     {
-        public Command GoLength { get; set; }
-        public Command GoForce { get; set; }
-        public Command GoMoment { get; set; }
-        public Command GoStress { get; set; }
+        public List<MenuItem> MenuItems { get; set; } = new List<MenuItem>();
         public MenuPageModel()
         {
-            GoLength = new Command(async () => await NavigateToLength());
-            GoForce = new Command(async () => await NavigateToForce());
-            GoMoment = new Command(async () => await NavigateToMoment());
-            GoStress = new Command(async () => await NavigateToStress());
+            MenuItems = new List<MenuItem>
+            {
+                new MenuItem{ Label="Length Converter",Image="Length.png",Navigate = new Command(async () => await NavigateToLength())},
+                new MenuItem{ Label="Force Converter",Image="Force.png",Navigate = new Command(async () => await NavigateToForce())},
+                new MenuItem{ Label="Moment Converter",Image="Moment.png",Navigate = new Command(async () => await NavigateToMoment())},
+                new MenuItem{ Label="Stress Converter",Image="Stress.png",Navigate = new Command(async () => await NavigateToStress())},
+
+            };
         }
         private async Task NavigateToLength()
         {
@@ -40,5 +42,12 @@ namespace AGECSUnitConvertor.PageModels.Convertors
             await CoreMethods.SwitchSelectedMaster<StressPageModel>();
         }
 
+    }
+
+    public class MenuItem
+    {
+        public string Label { get; set; }
+        public Command Navigate { get; set; }
+        public string Image { get; set; }
     }
 }
